1e6c2c9e2dde900a5e631925b81d7fef74cbb095
rakhagans37 <muhammadrakha3704@gmail.com>
2023-06-26 20:45:55 +0700

    Penambahan fungsi history dan icon history

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,36 +1,47 @@
 let darkmode = document.getElementById("checktheme");
+let buttonPackage = document.getElementById("button-package");
+let operation = document.getElementById("operation");
+let total = document.getElementById("total");
+let totalSymbol = document.getElementById("samadengan");
+let specialFunc = document.getElementById("special-func");
+let numberPackage = document.getElementById("number-package");
+let rightSide = document.getElementById("right-side");
+let historyImg = document.getElementById("historyImg");
+let historyDiv = document.getElementById("historyDiv");
+let historyList = document.getElementById("history-list");
+let arrayHistory = [];
+
 darkmode.addEventListener("change", () => {
    if (darkmode.checked) {
       document.body.classList.add("dark");
-      document
-         .getElementById("button-package")
-         .classList.add("darkbutton-package");
-      document.getElementById("samadengan").classList.add("dark");
-      document.getElementById("total").classList.add("dark");
-      document.getElementById("operation").classList.add("dark");
-      document.getElementById("special-func").classList.add("dark");
-      document.getElementById("number-package").classList.add("dark");
-      document.getElementById("right-side").classList.add("dark");
+      buttonPackage.classList.add("darkbutton-package");
+      totalSymbol.classList.add("dark");
+      total.classList.add("dark");
+      operation.classList.add("dark");
+      specialFunc.classList.add("dark");
+      numberPackage.classList.add("dark");
+      rightSide.classList.add("dark");
+      historyImg.src = "img/History.png";
+      historyDiv.classList.add("dark");
    } else {
       document.body.classList.remove("dark");
       document
          .getElementById("button-package")
          .classList.remove("darkbutton-package");
-      document.getElementById("samadengan").classList.remove("dark");
-      document.getElementById("total").classList.remove("dark");
-      document.getElementById("operation").classList.remove("dark");
-      document.getElementById("special-func").classList.remove("dark");
-      document.getElementById("number-package").classList.remove("dark");
-      document.getElementById("right-side").classList.remove("dark");
+      totalSymbol.classList.remove("dark");
+      total.classList.remove("dark");
+      operation.classList.remove("dark");
+      specialFunc.classList.remove("dark");
+      numberPackage.classList.remove("dark");
+      rightSide.classList.remove("dark");
+      historyImg.src = "img/Historyl.png";
+      historyDiv.classList.remove("dark");
    }
 });
 
-let operation = document.getElementById("operation");
-let total = document.getElementById("total");
-let totalSymbol = document.getElementById("samadengan");
-
 function insert(value) {
-   operation.textContent += value;
+   operation.textContent = "";
+   total.textContent += value;
 }
 
 function clearText() {
@@ -40,6 +51,42 @@ function clearText() {
 }
 
 function getTotal() {
-   total.textContent = eval(operation.textContent);
+   operation.textContent = total.textContent;
+   total.textContent = eval(total.textContent);
    totalSymbol.textContent = "=";
+   arrayHistory.push([operation.textContent, total.textContent]);
+   historyPrint(operation.textContent, total.textContent);
+}
+
+let using = 0;
+function historyOn() {
+   using += 1;
+   if (using % 2 == 1) {
+      historyList.style.visibility = "visible";
+   } else {
+      historyList.style.visibility = "hidden";
+   }
+}
+
+function historyPrint(operationString, totalString) {
+   const createDiv = document.createElement("div");
+   const createOp = document.createElement("p");
+   const createSamaDengan = document.createElement("p");
+   const createTotal = document.createElement("p");
+   const createDivTotal = document.createElement("div");
+
+   createOp.textContent = operationString;
+   createSamaDengan.textContent = "=";
+   createTotal.textContent = totalString;
+
+   createDiv.classList.add("history-unit");
+   createDiv.setAttribute("onclick", "historyCopy()");
+   createOp.classList.add("operation");
+   createDivTotal.classList.add("history-print");
+
+   historyList.appendChild(createDiv);
+   createDiv.appendChild(createOp);
+   createDiv.appendChild(createDivTotal);
+   createDivTotal.appendChild(createSamaDengan);
+   createDivTotal.appendChild(createTotal);
 }
